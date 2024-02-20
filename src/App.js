b4dc1e9c10f09350035915646c1a15901e31a293
Robin Gouda <robin2contact@gmail.com>
2024-02-20 09:48:17 +0530

    node module modified

--- src/App.js
+++ src/App.js
@@ -1,4 +1,18 @@
+import { useState, useEffect } from "react";
+
+import { fetchDataFromApi } from "./Utilities/api.js";
+
 function App() {
+  const apiTesting = () => {
+    fetchDataFromApi(`/movie/popular`).then((res) => {
+      console.log(res);
+    });
+  };
+
+  useEffect(() => {
+    apiTesting();
+  }, []);
+
   return (
     <div className="App">
       <h1> Hello</h1>
